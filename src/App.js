374e8d533d8d5d78ddc2be31e4b080a7898fcef3
Oscar Islas <oislasreyes@gmail.com>
2020-02-26 22:43:31 -0600

    Iniciando nuestra aplicación de react con un componente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Card} from './components/card/card-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      personas: [
+        {
+          name: "Oscar Islas",
+          email: "oislasreyes@gmail.com"
+        },
+        {
+          name: "Eduardo",
+          email: "eduardo@gmail.com"
+        }
+      ],
+      nombre: "Mi primera app"
+    }
+    let {personas, nombre} = this.state;
+    console.log(nombre);
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Card />
+        {this.state.personas.map( persona => <h1>{persona.name}</h1>)}        
+      </div>
+      
+    );
+  } 
 }
 
 export default App;
---
+++ src/components/card/card-component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Card = props => (
+  <div className='card-container'>
+    <h1>Hello World</h1>
+  </div>
+);
